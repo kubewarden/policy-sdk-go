d96bbea70d788b6bb6ad14b6ae1f0fbe0de6d527
Flavio Castelli <fcastelli@suse.com>
2023-03-07 15:49:09 +0100

    feat: add kubernetes context aware API
    Add the kubernetes context aware API.
    This includes the new Go types used to exchange information.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- host_capabilities/host.go
+++ host_capabilities/host.go
@@ -139,3 +139,54 @@ func (h *Host) VerifyKeyless(image string, keyless []KeylessInfo, annotations ma
 
 	return responseObj, nil
 }
+
+// ListResourcesByNamespace gets all the Kubernetes resources defined inside of
+// the given namespace
+// Note: cannot be used for cluster-wide resources
+func (h *Host) ListResourcesByNamespace(req ListResourcesByNamespaceRequest) ([]byte, error) {
+	payload, err := req.MarshalJSON()
+	if err != nil {
+		return []byte{}, fmt.Errorf("cannot serialize request object: %w", err)
+	}
+
+	// perform callback
+	responsePayload, err := h.Client.HostCall("kubewarden", "kubernetes", "list_resources_by_namespace", payload)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return responsePayload, nil
+}
+
+// ListResources gets all the Kubernetes resources defined inside of the cluster.
+// Note: this has be used for cluster-wide resources
+func (h *Host) ListResources(req ListAllResourcesRequest) ([]byte, error) {
+	payload, err := req.MarshalJSON()
+	if err != nil {
+		return []byte{}, fmt.Errorf("cannot serialize request object: %w", err)
+	}
+
+	// perform callback
+	responsePayload, err := h.Client.HostCall("kubewarden", "kubernetes", "list_all_resources", payload)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return responsePayload, nil
+}
+
+// GetResource gets a specific Kubernetes resource.
+func (h *Host) GetResource(req GetResourceRequest) ([]byte, error) {
+	payload, err := req.MarshalJSON()
+	if err != nil {
+		return []byte{}, fmt.Errorf("cannot serialize request object: %w", err)
+	}
+
+	// perform callback
+	responsePayload, err := h.Client.HostCall("kubewarden", "kubernetes", "get_resource", payload)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return responsePayload, nil
+}
