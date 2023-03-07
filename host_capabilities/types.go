c100f57b6a793c2c590df000de3692328f0776f7
Flavio Castelli <fcastelli@suse.com>
2023-03-07 15:49:09 +0100

    feat: add kubernetes context aware API
    Add the kubernetes context aware API.
    This includes the new Go types used to exchange information.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- host_capabilities/types.go
+++ host_capabilities/types.go
@@ -61,3 +61,50 @@ type LookupHostResponse struct {
 	// List of IP addresses associated with the host
 	Ips []string `json:"ips"`
 }
+
+// Set of parameters used by the `list_resources_by_namespace` function
+type ListResourcesByNamespaceRequest struct {
+	// apiVersion of the resource (v1 for core group, groupName/groupVersions for other).
+	APIVersion string `json:"api_version"`
+	// Singular PascalCase name of the resource
+	Kind string `json:"kind"`
+	// Namespace scoping the search
+	Namespace string `json:"namespace"`
+	// A selector to restrict the list of returned objects by their labels.
+	// Defaults to everything if omitted
+	LabelSelector string `json:"label_selector,omitempty"`
+	// A selector to restrict the list of returned objects by their fields.
+	// Defaults to everything if omitted
+	FieldSelector string `json:"field_selector,omitempty"`
+}
+
+// Set of parameters used by the `list_all_resources` function
+type ListAllResourcesRequest struct {
+	// apiVersion of the resource (v1 for core group, groupName/groupVersions for other).
+	APIVersion string `json:"api_version"`
+	// Singular PascalCase name of the resource
+	Kind string `json:"kind"`
+	// A selector to restrict the list of returned objects by their labels.
+	// Defaults to everything if omitted
+	LabelSelector string `json:"label_selector,omitempty"`
+	// A selector to restrict the list of returned objects by their fields.
+	// Defaults to everything if omitted
+	FieldSelector string `json:"field_selector,omitempty"`
+}
+
+// Set of parameters used by the `get_resource` function
+type GetResourceRequest struct {
+	APIVersion string `json:"api_version"`
+	// Singular PascalCase name of the resource
+	Kind string `json:"kind"`
+	// Namespace scoping the search
+	Namespace string `json:"namespace"`
+	// The name of the resource
+	Name string `json:"name"`
+	// Disable caching of results obtained from Kubernetes API Server
+	// By default query results are cached for 5 seconds, that might cause
+	// stale data to be returned.
+	// However, making too many requests against the Kubernetes API Server
+	// might cause issues to the cluster
+	DisableCache bool `json:"disable_cache"`
+}
--- host_capabilities/types_easyjson.go
+++ host_capabilities/types_easyjson.go
@@ -542,7 +542,188 @@ func (v *LookupHostResponse) UnmarshalJSON(data []byte) error {
 func (v *LookupHostResponse) UnmarshalEasyJSON(l *jlexer.Lexer) {
 	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities4(l, v)
 }
-func easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities5(in *jlexer.Lexer, out *KeylessInfo) {
+func easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities5(in *jlexer.Lexer, out *ListResourcesByNamespaceRequest) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeFieldName(false)
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "api_version":
+			out.APIVersion = string(in.String())
+		case "kind":
+			out.Kind = string(in.String())
+		case "namespace":
+			out.Namespace = string(in.String())
+		case "label_selector":
+			out.LabelSelector = string(in.String())
+		case "field_selector":
+			out.FieldSelector = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities5(out *jwriter.Writer, in ListResourcesByNamespaceRequest) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"api_version\":"
+		out.RawString(prefix[1:])
+		out.String(string(in.APIVersion))
+	}
+	{
+		const prefix string = ",\"kind\":"
+		out.RawString(prefix)
+		out.String(string(in.Kind))
+	}
+	{
+		const prefix string = ",\"namespace\":"
+		out.RawString(prefix)
+		out.String(string(in.Namespace))
+	}
+	if in.LabelSelector != "" {
+		const prefix string = ",\"label_selector\":"
+		out.RawString(prefix)
+		out.String(string(in.LabelSelector))
+	}
+	if in.FieldSelector != "" {
+		const prefix string = ",\"field_selector\":"
+		out.RawString(prefix)
+		out.String(string(in.FieldSelector))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v ListResourcesByNamespaceRequest) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities5(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v ListResourcesByNamespaceRequest) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities5(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *ListResourcesByNamespaceRequest) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities5(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *ListResourcesByNamespaceRequest) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities5(l, v)
+}
+func easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities6(in *jlexer.Lexer, out *ListAllResourcesRequest) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeFieldName(false)
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "api_version":
+			out.APIVersion = string(in.String())
+		case "kind":
+			out.Kind = string(in.String())
+		case "label_selector":
+			out.LabelSelector = string(in.String())
+		case "field_selector":
+			out.FieldSelector = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities6(out *jwriter.Writer, in ListAllResourcesRequest) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"api_version\":"
+		out.RawString(prefix[1:])
+		out.String(string(in.APIVersion))
+	}
+	{
+		const prefix string = ",\"kind\":"
+		out.RawString(prefix)
+		out.String(string(in.Kind))
+	}
+	if in.LabelSelector != "" {
+		const prefix string = ",\"label_selector\":"
+		out.RawString(prefix)
+		out.String(string(in.LabelSelector))
+	}
+	if in.FieldSelector != "" {
+		const prefix string = ",\"field_selector\":"
+		out.RawString(prefix)
+		out.String(string(in.FieldSelector))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v ListAllResourcesRequest) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities6(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v ListAllResourcesRequest) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities6(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *ListAllResourcesRequest) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities6(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *ListAllResourcesRequest) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities6(l, v)
+}
+func easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities7(in *jlexer.Lexer, out *KeylessInfo) {
 	isTopLevel := in.IsStart()
 	if in.IsNull() {
 		if isTopLevel {
@@ -575,7 +756,7 @@ func easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities5(in *j
 		in.Consumed()
 	}
 }
-func easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities5(out *jwriter.Writer, in KeylessInfo) {
+func easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities7(out *jwriter.Writer, in KeylessInfo) {
 	out.RawByte('{')
 	first := true
 	_ = first
@@ -595,23 +776,117 @@ func easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities5(out *
 // MarshalJSON supports json.Marshaler interface
 func (v KeylessInfo) MarshalJSON() ([]byte, error) {
 	w := jwriter.Writer{}
-	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities5(&w, v)
+	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities7(&w, v)
 	return w.Buffer.BuildBytes(), w.Error
 }
 
 // MarshalEasyJSON supports easyjson.Marshaler interface
 func (v KeylessInfo) MarshalEasyJSON(w *jwriter.Writer) {
-	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities5(w, v)
+	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities7(w, v)
 }
 
 // UnmarshalJSON supports json.Unmarshaler interface
 func (v *KeylessInfo) UnmarshalJSON(data []byte) error {
 	r := jlexer.Lexer{Data: data}
-	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities5(&r, v)
+	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities7(&r, v)
 	return r.Error()
 }
 
 // UnmarshalEasyJSON supports easyjson.Unmarshaler interface
 func (v *KeylessInfo) UnmarshalEasyJSON(l *jlexer.Lexer) {
-	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities5(l, v)
+	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities7(l, v)
+}
+func easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities8(in *jlexer.Lexer, out *GetResourceRequest) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeFieldName(false)
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "api_version":
+			out.APIVersion = string(in.String())
+		case "kind":
+			out.Kind = string(in.String())
+		case "namespace":
+			out.Namespace = string(in.String())
+		case "name":
+			out.Name = string(in.String())
+		case "disable_cache":
+			out.DisableCache = bool(in.Bool())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities8(out *jwriter.Writer, in GetResourceRequest) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"api_version\":"
+		out.RawString(prefix[1:])
+		out.String(string(in.APIVersion))
+	}
+	{
+		const prefix string = ",\"kind\":"
+		out.RawString(prefix)
+		out.String(string(in.Kind))
+	}
+	{
+		const prefix string = ",\"namespace\":"
+		out.RawString(prefix)
+		out.String(string(in.Namespace))
+	}
+	{
+		const prefix string = ",\"name\":"
+		out.RawString(prefix)
+		out.String(string(in.Name))
+	}
+	{
+		const prefix string = ",\"disable_cache\":"
+		out.RawString(prefix)
+		out.Bool(bool(in.DisableCache))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v GetResourceRequest) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities8(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v GetResourceRequest) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson6601e8cdEncodeGithubComKubewardenPolicySdkGoHostCapabilities8(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *GetResourceRequest) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities8(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *GetResourceRequest) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson6601e8cdDecodeGithubComKubewardenPolicySdkGoHostCapabilities8(l, v)
 }
