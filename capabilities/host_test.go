0b80f7587fc25a8f90aee1a8d9c80b516c16494c
Khaled Emara <mail@KhaledEmara.dev>
2023-04-19 00:28:31 +0200

    feat: add certificate verification and Unit Tests
    Add Certificates Verification APIs.
    Add Unit Tests for all the host capabilities APIs.
    
    Signed-off-by: Khaled Emara <mail@KhaledEmara.dev>

--- capabilities/host_test.go
+++ capabilities/host_test.go
@@ -1,6 +1,7 @@
 package capabilities
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -239,4 +240,117 @@ func CheckKeylessTrustedV1(host Host, request easyjson.Marshaler) (bool, error) 
 		return false, err
 	}
 	return res.IsTrusted, nil
+}
+
+func TestV1IsCertificateTrusted(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	m := mock_capabilities.NewMockWapcClient(ctrl)
+
+	cert := Certificate{
+		Encoding: Pem,
+		Data:     []rune(`certificate0`),
+	}
+	chain := []Certificate{{
+		Encoding: Pem,
+		Data:     []rune(`certificate1`),
+	}, {
+		Encoding: Pem,
+		Data:     []rune(`certificate2`),
+	}}
+	not_after := "2021-10-01T00:00:00Z"
+
+	verificationResponse := CertificateVerificationResponse{
+		Trusted: true,
+		Reason:  "",
+	}
+	verificationPayload, err := easyjson.Marshal(verificationResponse)
+	if err != nil {
+		t.Fatalf("cannot serialize response object: %v", err)
+	}
+
+	expectedPayload := []byte{123, 34, 99, 101, 114, 116, 34, 58, 123, 34, 101, 110, 99, 111, 100, 105, 110, 103, 34, 58, 34, 80, 101, 109, 34, 44, 34, 100, 97, 116, 97, 34, 58, 91, 57, 57, 44, 49, 48, 49, 44, 49, 49, 52, 44, 49, 49, 54, 44, 49, 48, 53, 44, 49, 48, 50, 44, 49, 48, 53, 44, 57, 57, 44, 57, 55, 44, 49, 49, 54, 44, 49, 48, 49, 44, 52, 56, 93, 125, 44, 34, 99, 101, 114, 116, 95, 99, 104, 97, 105, 110, 34, 58, 91, 123, 34, 101, 110, 99, 111, 100, 105, 110, 103, 34, 58, 34, 80, 101, 109, 34, 44, 34, 100, 97, 116, 97, 34, 58, 91, 57, 57, 44, 49, 48, 49, 44, 49, 49, 52, 44, 49, 49, 54, 44, 49, 48, 53, 44, 49, 48, 50, 44, 49, 48, 53, 44, 57, 57, 44, 57, 55, 44, 49, 49, 54, 44, 49, 48, 49, 44, 52, 57, 93, 125, 44, 123, 34, 101, 110, 99, 111, 100, 105, 110, 103, 34, 58, 34, 80, 101, 109, 34, 44, 34, 100, 97, 116, 97, 34, 58, 91, 57, 57, 44, 49, 48, 49, 44, 49, 49, 52, 44, 49, 49, 54, 44, 49, 48, 53, 44, 49, 48, 50, 44, 49, 48, 53, 44, 57, 57, 44, 57, 55, 44, 49, 49, 54, 44, 49, 48, 49, 44, 53, 48, 93, 125, 93, 44, 34, 110, 111, 116, 95, 97, 102, 116, 101, 114, 34, 58, 34, 50, 48, 50, 49, 45, 49, 48, 45, 48, 49, 84, 48, 48, 58, 48, 48, 58, 48, 48, 90, 34, 125}
+
+	m.
+		EXPECT().
+		HostCall("kubewarden", "crypto", "v1/is_certificate_trusted", expectedPayload).
+		Return(verificationPayload, nil).
+		Times(1)
+
+	host := Host{
+		Client: m,
+	}
+
+	res, err := host.VerifyCert(cert, chain, not_after)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !res {
+		t.Fatalf("expected trusted image, got untrusted")
+	}
+}
+
+func TestV1ManifestDigest(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	m := mock_capabilities.NewMockWapcClient(ctrl)
+
+	digestResponse := OciManifestResponse{
+		Digest: "myhash",
+	}
+	digestPayload, err := json.Marshal(digestResponse)
+	if err != nil {
+		t.Fatalf("cannot serialize response object: %v", err)
+	}
+
+	expectedPayload := []byte{34, 109, 121, 105, 109, 97, 103, 101, 58, 108, 97, 116, 101, 115, 116, 34}
+
+	m.
+		EXPECT().
+		HostCall("kubewarden", "oci", "v1/manifest_digest", expectedPayload).
+		Return(digestPayload, nil).
+		Times(1)
+
+	host := Host{
+		Client: m,
+	}
+
+	res, err := host.GetOCIManifestDigest("myimage:latest")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if res != digestResponse.Digest {
+		t.Fatalf("unexpected error")
+	}
+}
+
+func TestV1DnsLookupHost(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	m := mock_capabilities.NewMockWapcClient(ctrl)
+
+	lookupResponse := LookupHostResponse{
+		Ips: []string{"127.0.0.1"},
+	}
+	lookupPayload, err := json.Marshal(lookupResponse)
+	if err != nil {
+		t.Fatalf("cannot serialize response object: %v", err)
+	}
+
+	expectedPayload := []byte{34, 108, 111, 99, 97, 108, 104, 111, 115, 116, 34}
+
+	m.
+		EXPECT().
+		HostCall("kubewarden", "net", "v1/dns_lookup_host", expectedPayload).
+		Return(lookupPayload, nil).
+		Times(1)
+
+	host := Host{
+		Client: m,
+	}
+
+	res, err := host.LookupHost("localhost")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if res[0] != lookupResponse.Ips[0] {
+		t.Fatalf("unexpected error")
+	}
 }
