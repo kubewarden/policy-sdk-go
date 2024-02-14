16125dab3d71204dacd2cc30ebfb0d5315581b66
José Guilherme Vanz <jguilhermevanz@suse.com>
2024-02-14 17:06:55 -0300

    Refactor tests
    Refactors the unit tests for the manifest capability. Merging all the
    previous 3 tests into a single test with sub tests.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

--- pkg/capabilities/oci/manifest/manifest_test.go
+++ pkg/capabilities/oci/manifest/manifest_test.go
@@ -3,6 +3,7 @@ package manifest
 import (
 	_ "crypto/sha256"
 	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -23,27 +24,95 @@ func buildHostMock(t *testing.T, expectedPayload []byte, returnPayload []byte) *
 	}
 }
 
-func TestV1OciImageManifest(t *testing.T) {
-	tests := []string{specs.MediaTypeImageManifest, constants.ImageManifestMediaType}
-	for _, mediaType := range tests {
-		t.Run(mediaType, func(t *testing.T) {
-			imageManifest := specs.Manifest{
-				MediaType: mediaType,
-				Config: specs.Descriptor{
-					MediaType:   specs.MediaTypeDescriptor,
-					Digest:      digest.FromString("mydummydigest"),
-					Size:        1024,
-					URLs:        []string{"ghcr.io/kubewarden/policy-server:latest"},
-					Annotations: map[string]string{"annotation": "value"},
-					Platform: &specs.Platform{
-						Architecture: "amd64",
-						OS:           "linux",
-					},
-				},
-				Layers:      []specs.Descriptor{},
-				Annotations: map[string]string{"annotation": "value"},
-			}
-			manifestPayload, err := json.Marshal(imageManifest)
+func buildManifest(mediaType string) interface{} {
+	return specs.Manifest{
+		MediaType: mediaType,
+		Config: specs.Descriptor{
+			MediaType:   mediaType,
+			Digest:      digest.FromString("mydummydigest"),
+			Size:        1024,
+			URLs:        []string{"ghcr.io/kubewarden/policy-server:latest"},
+			Annotations: map[string]string{"annotation": "value"},
+			Platform: &specs.Platform{
+				Architecture: "amd64",
+				OS:           "linux",
+			},
+		},
+		Layers:      []specs.Descriptor{},
+		Annotations: map[string]string{"annotation": "value"},
+	}
+}
+
+func buildIndexManifest(mediaType string) interface{} {
+	return specs.Index{
+		MediaType: mediaType,
+		Manifests: []specs.Descriptor{{
+			MediaType:   specs.MediaTypeDescriptor,
+			Digest:      digest.FromString("mydummydigest"),
+			Size:        1024,
+			URLs:        []string{"ghcr.io/kubewarden/policy-server:latest"},
+			Annotations: map[string]string{"annotation": "value"},
+			Platform: &specs.Platform{
+				Architecture: "amd64",
+				OS:           "linux",
+			},
+		},
+		},
+		// Annotations contains arbitrary metadata for the image index.
+		Annotations: map[string]string{"annonation": "annotationValue"},
+	}
+}
+
+func TestV1OciManifest(t *testing.T) {
+
+	tests := []struct {
+		manifest                     interface{}
+		failsBecauseUnknownMediaType bool
+	}{
+		{
+			manifest:                     buildManifest(specs.MediaTypeImageManifest),
+			failsBecauseUnknownMediaType: false,
+		},
+		{
+			manifest:                     buildManifest(constants.ImageManifestMediaType),
+			failsBecauseUnknownMediaType: false,
+		},
+		{
+			manifest:                     buildIndexManifest(specs.MediaTypeImageIndex),
+			failsBecauseUnknownMediaType: false,
+		},
+		{
+			manifest:                     buildIndexManifest(constants.ImageManifestListMediaType),
+			failsBecauseUnknownMediaType: false,
+		},
+		{
+			manifest:                     buildIndexManifest(specs.MediaTypeDescriptor),
+			failsBecauseUnknownMediaType: true,
+		},
+		{
+			manifest:                     buildIndexManifest(specs.MediaTypeImageConfig),
+			failsBecauseUnknownMediaType: true,
+		},
+		{
+			manifest:                     buildIndexManifest(specs.MediaTypeImageLayerGzip),
+			failsBecauseUnknownMediaType: true,
+		},
+		{
+			manifest:                     buildManifest(specs.MediaTypeDescriptor),
+			failsBecauseUnknownMediaType: true,
+		},
+		{
+			manifest:                     buildManifest(specs.MediaTypeImageConfig),
+			failsBecauseUnknownMediaType: true,
+		},
+		{
+			manifest:                     buildManifest(specs.MediaTypeImageLayerGzip),
+			failsBecauseUnknownMediaType: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			manifestPayload, err := json.Marshal(test.manifest)
 			if err != nil {
 				t.Fatalf("cannot serialize response object: %v", err)
 			}
@@ -51,93 +120,34 @@ func TestV1OciImageManifest(t *testing.T) {
 			host := buildHostMock(t, []byte(expectedPayload), manifestPayload)
 
 			res, err := GetOCIManifest(host, "myimage:latest")
+
+			if test.failsBecauseUnknownMediaType {
+				if err == nil || !strings.Contains(err.Error(), "not a valid media type") {
+					t.Fatal("expected an invalid media type error")
+				}
+				return
+			}
 			if err != nil {
 				t.Fatalf("unexpected error: %v", err)
 			}
 
-			if res.IndexManifest() != nil {
-				t.Fatal("response should not be an index manifest")
-			}
-			if diff := cmp.Diff(*res.ImageManifest(), imageManifest); diff != "" {
-				t.Fatalf("unexpected image manifest:\n%s", diff)
-			}
-		})
-	}
-}
-
-func TestV1OciIndexManifest(t *testing.T) {
-	tests := []string{specs.MediaTypeImageIndex, constants.ImageManifestListMediaType}
-	for _, mediaType := range tests {
-		t.Run(mediaType, func(t *testing.T) {
-			indexManifest := specs.Index{
-				MediaType: mediaType,
-				Manifests: []specs.Descriptor{{
-					MediaType:   specs.MediaTypeDescriptor,
-					Digest:      digest.FromString("mydummydigest"),
-					Size:        1024,
-					URLs:        []string{"ghcr.io/kubewarden/policy-server:latest"},
-					Annotations: map[string]string{"annotation": "value"},
-					Platform: &specs.Platform{
-						Architecture: "amd64",
-						OS:           "linux",
-					},
-				},
-				},
-				// Annotations contains arbitrary metadata for the image index.
-				Annotations: map[string]string{"annonation": "annotationValue"},
-			}
-			manifestPayload, err := json.Marshal(indexManifest)
-			if err != nil {
-				t.Fatalf("cannot serialize response object: %v", err)
-			}
-			expectedPayload := `"myimage:latest"`
-			host := buildHostMock(t, []byte(expectedPayload), manifestPayload)
-
-			res, err := GetOCIManifest(host, "myimage:latest")
-			if err != nil {
-				t.Fatalf("unexpected error: %v", err)
-			}
-			if res.ImageManifest() != nil {
-				t.Fatalf("response should not be an image manifest:\n%v", res.ImageManifest())
-			}
-			if diff := cmp.Diff(*res.IndexManifest(), indexManifest); diff != "" {
-				t.Fatalf("unexpected index manifest:\n%s", diff)
-			}
-		})
-	}
-}
-
-func TestV1OciManifestInvalidMediaTypes(t *testing.T) {
-	tests := []string{specs.MediaTypeDescriptor, specs.MediaTypeImageConfig, specs.MediaTypeImageLayerGzip}
-	for _, mediaType := range tests {
-		t.Run(mediaType, func(t *testing.T) {
-			indexManifest := specs.Index{
-				MediaType: mediaType,
-				Manifests: []specs.Descriptor{{
-					MediaType:   specs.MediaTypeDescriptor,
-					Digest:      digest.FromString("mydummydigest"),
-					Size:        1024,
-					URLs:        []string{"ghcr.io/kubewarden/policy-server:latest"},
-					Annotations: map[string]string{"annotation": "value"},
-					Platform: &specs.Platform{
-						Architecture: "amd64",
-						OS:           "linux",
-					},
-				},
-				},
-				// Annotations contains arbitrary metadata for the image index.
-				Annotations: map[string]string{"annonation": "annotationValue"},
-			}
-			manifestPayload, err := json.Marshal(indexManifest)
-			if err != nil {
-				t.Fatalf("cannot serialize response object: %v", err)
-			}
-			expectedPayload := `"myimage:latest"`
-			host := buildHostMock(t, []byte(expectedPayload), manifestPayload)
-
-			_, err = GetOCIManifest(host, "myimage:latest")
-			if err == nil {
-				t.Fatal("expected error")
+			switch test.manifest.(type) {
+			case specs.Manifest:
+				if res.IndexManifest() != nil {
+					t.Fatal("response should not be an index manifest")
+				}
+				if diff := cmp.Diff(*res.ImageManifest(), test.manifest); diff != "" {
+					t.Fatalf("unexpected manifest:\n%s", diff)
+				}
+			case specs.Index:
+				if res.ImageManifest() != nil {
+					t.Fatalf("response should not be an image manifest:\n%v", res.ImageManifest())
+				}
+				if diff := cmp.Diff(*res.IndexManifest(), test.manifest); diff != "" {
+					t.Fatalf("unexpected manifest:\n%s", diff)
+				}
+			default:
+				t.Fatal("unexpected manifest")
 			}
 		})
 	}
