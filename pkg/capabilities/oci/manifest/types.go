13011ccb2479a575a1eab72467ff4cdb0a0ec383
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2024-04-23 12:19:42 +0200

    feat!: Export OciImageManifestResponse{} fields
    This is useful for test mockups on the cel-policy.
    
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- pkg/capabilities/oci/manifest/types.go
+++ pkg/capabilities/oci/manifest/types.go
@@ -3,37 +3,40 @@ package manifest
 import (
 	"encoding/json"
 	"fmt"
+
 	"github.com/kubewarden/policy-sdk-go/constants"
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 type OciImageManifestResponse struct {
-	image *specs.Manifest
-	index *specs.Index
+	Image *specs.Manifest `json:"image"`
+	Index *specs.Index    `json:"index"`
 }
 
 func (r OciImageManifestResponse) ImageManifest() *specs.Manifest {
-	return r.image
+	return r.Image
 }
 
 func (r OciImageManifestResponse) IndexManifest() *specs.Index {
-	return r.index
+	return r.Index
 }
 
 func (r *OciImageManifestResponse) UnmarshalJSON(b []byte) error {
 	imageManifest := specs.Manifest{}
 	if err := json.Unmarshal(b, &imageManifest); err == nil {
 		if isImageMediaType(imageManifest.MediaType) {
-			r.image = &imageManifest
+			r.Image = &imageManifest
 			return nil
 		}
+		fmt.Printf("%+v\n", imageManifest)
 	}
 	indexManifest := specs.Index{}
 	if err := json.Unmarshal(b, &indexManifest); err == nil {
 		if isImageIndexMediaType(indexManifest.MediaType) {
-			r.index = &indexManifest
+			r.Index = &indexManifest
 			return nil
 		}
+		fmt.Printf("%+v\n", indexManifest)
 		return fmt.Errorf("not a valid media type: %s", indexManifest.MediaType)
 	}
 	return fmt.Errorf("cannot decode response")
